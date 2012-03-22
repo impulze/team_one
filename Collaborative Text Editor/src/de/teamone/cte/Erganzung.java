8a392e4f9b3e22af1110555ae886ce74170809f4
Hicran <hicran@gerikoglu.com>
2012-03-22 22:26:26 +0100

    sauber

--- Collaborative Text Editor/src/de/teamone/cte/Erganzung.java
+++ Collaborative Text Editor/src/de/teamone/cte/Erganzung.java
@@ -1,13 +1,5 @@
 package de.teamone.cte;
 
 public class Erganzung {
-//<<<<<<< HEAD
-	
-//=======
-	// hier findet jetzt mal ne Änderung statt
-	// fixed :)
-
-//>>>>>>> f1d2886a7fc3283ee74f0f8997831eb93e27f660
-//
-	// ab hier fängt dann mein text an 
+// sauber
 }
