0e9d4655b3cb1024363fa3070df2fce49bcd6b95
Hicran <hicran@gerikoglu.com>
2012-03-22 22:24:03 +0100

    alles auskommentiert und ein kommentar hinzugefügt

--- Collaborative Text Editor/src/de/teamone/cte/Erganzung.java
+++ Collaborative Text Editor/src/de/teamone/cte/Erganzung.java
@@ -1,11 +1,13 @@
 package de.teamone.cte;
 
 public class Erganzung {
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	
-=======
+//=======
 	// hier findet jetzt mal ne Änderung statt
 	// fixed :)
-	// wieder was drin
->>>>>>> f1d2886a7fc3283ee74f0f8997831eb93e27f660
+
+//>>>>>>> f1d2886a7fc3283ee74f0f8997831eb93e27f660
+//
+	// ab hier fängt dann mein text an 
 }
