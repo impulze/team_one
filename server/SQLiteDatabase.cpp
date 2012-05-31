fc5dfe5a939cf7d6d037139c35f5476b5f100f77
Daniel Mierswa <impulze@impulze.org>
2012-05-31 13:49:52 +0200

    SQLiteDatabase: first implementation of executing sql statements

--- server/SQLiteDatabase.cpp
+++ server/SQLiteDatabase.cpp
@@ -1,5 +1,6 @@
 #include "SQLiteDatabase.h"
 
+#include <cassert>
 #include <iostream>
 
 #include <sqlite3.h>
@@ -15,6 +16,13 @@ namespace database_errors
 		: SQLiteError(message)
 	{
 	}
+}
+
+namespace
+{
+	int execute_sql_callback(void *results, int columns,
+	                         char **column_results,
+	                         char **column_names);
 }
 
 SQLiteDatabase::SQLiteDatabase(std::string const &path)
@@ -48,10 +56,76 @@ bool SQLiteDatabase::complete_sql(std::string const &statement) const
 	return sqlite3_complete(statement.c_str()) != 0;
 }
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
 SQLiteDatabase::results_t SQLiteDatabase::execute_sql(std::string const &statement)
 {
-	throw std::logic_error("unimplemented");
+	using database_errors::SQLiteError;
+
+	results_t results;
+	char *sqlite_error_string;
+	int const result = sqlite3_exec(
+		handle_, statement.c_str(),
+		execute_sql_callback, &results,
+		&sqlite_error_string);
+
+	/**
+	 * SQLiteFree is a class which only exists to free the resources
+	 * allocated by sqlite3 in this scope.
+	 * Even if this implementation throws an error the constructor
+	 * of this class is called and the resources properly freed.
+	 */
+	struct SQLiteFree
+	{
+		SQLiteFree(char *error_string)
+			: error_string(error_string)
+		{
+		}
+
+		~SQLiteFree()
+		{
+			sqlite3_free(error_string);
+		}
+
+		char *error_string;
+	};
+
+	SQLiteFree sqlite_free_object(sqlite_error_string);
+
+	if (result)
+	{
+		if (sqlite_error_string)
+		{
+			throw SQLiteError(sqlite_error_string);
+		}
+		else
+		{
+			throw SQLiteError("unknown SQLite error");
+		}
+	}
+
+	return results;
 }
-#pragma GCC diagnostic pop
+
+namespace
+{
+	int execute_sql_callback(void *results_ptr, int columns,
+	                         char **column_results,
+	                         char **column_names)
+	{
+		Database::result_t result;
+
+		for (int i = 0; i < columns; i++)
+		{
+			// every column should have a non-NULL name
+			assert(column_names[i]);
+
+			result[column_names[i]] = column_results[i];
+		}
+
+		Database::results_t &results =
+			*static_cast<Database::results_t *>(results_ptr);
+
+		results.push_back(result);
+
+		return 0;
+	}
+}
