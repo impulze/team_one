310ba4373ecb58d474d4cc1651cbd3a21e5cc283
Daniel Mierswa <impulze@impulze.org>
2012-06-06 16:22:32 +0200

    UserInterface: explain get_input, make non-copyable

--- server/UserInterface.h
+++ server/UserInterface.h
@@ -15,6 +15,37 @@
 class UserInterface
 {
 public:
+	/**
+	 * Construct a user command line interface object.
+	 * Provide a default constructor, because this is just an interface.
+	 */
+	UserInterface() = default;
+
+	/**
+	 * Deconstruct a user command line interface object, freeing all its resources
+	 * and probably closing the window if any.
+	 */
+	virtual ~UserInterface();
+
+	/**
+	 * Delete the default copy constructor, making copying a user command line
+	 * interface object impossible.
+	 */
+	UserInterface(UserInterface const &) = delete;
+
+	/**
+	 * Delete the default assignment operator, making assigning a user command line
+	 * interface object impossible.
+	 */
+	UserInterface &operator=(UserInterface const &) = delete;
+
+	/**
+	 * Obtain one line of input. Block until it's received.
+	 *
+	 * @return The line of input which was typed with newline removed.
+	 * @throws std::runtime_error If processing in the underlying
+	 *                            implementation failed.
+	 */
 	virtual std::string get_input() = 0;
 };
 
