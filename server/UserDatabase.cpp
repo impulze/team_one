043681604e21ca4a650e6b74d86f3740eca13f79
Daniel Mierswa <impulze@impulze.org>
2012-06-20 15:32:31 +0200

    UserDatabase: provide hash_bytes

--- server/UserDatabase.cpp
+++ server/UserDatabase.cpp
@@ -2,6 +2,8 @@
 
 #include "UserInterface.h"
 
+#include <openssl/sha.h>
+
 #include <stdexcept>
 
 /**
@@ -33,7 +35,17 @@ void UserDatabase::remove(std::string const &)
 	throw std::runtime_error("STUB UserDatabase::remove");
 }
 
-UserDatabase::password_hash_t UserDatabase::hash_bytes(std::vector<char> const &)
+UserDatabase::password_hash_t UserDatabase::hash_bytes(std::vector<char> const &bytes)
 {
-	throw std::runtime_error("STUB UserDatabase::hash_bytes");
+	if (bytes.size() >= std::numeric_limits<unsigned long>::max())
+	{
+		throw std::runtime_error("unable to create hash because byte container is too big");
+	}
+
+	password_hash_t sha1_hash;
+
+	// should never fail
+	::SHA1(reinterpret_cast<unsigned char const *>(&bytes[0]), bytes.size(), &sha1_hash[0]);
+
+	return sha1_hash;
 }
