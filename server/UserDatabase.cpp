3ba8d6212481c80a8e821a47d669219cda87237f
Daniel Mierswa <impulze@impulze.org>
2012-06-20 10:44:59 +0200

    UserDatabase: provide more sql calls (still stub calls)

--- server/UserDatabase.cpp
+++ server/UserDatabase.cpp
@@ -13,25 +13,81 @@
  * Implementation file for the user database implementation.
  */
 
+namespace
+{
+	std::string const g_sql_queries[] = {
+		"CREATE TABLE IF NOT EXISTS UserDatabase ("
+			"u_id INTEGER NOT NULL,"
+			"u_name VARCHAR(64) NOT NULL,"
+			"u_pwhash VARCHAR(20) NOT NULL,"
+			"PRIMARY KEY(u_id),"
+			"UNIQUE(u_name)"
+		");",
+		"SELECT u_pwhash FROM UserDatabase WHERE u_name = %Q;",
+		"INSERT INTO UserDatabase VALUES (%Q, %Q);",
+		"DELETE FROM UserDatabase WHERE p_name = %Q;",
+	};
+}
+
 UserDatabase::UserDatabase(std::shared_ptr<Database> database,
                            UserInterface &user_interface)
 	: database_(database),
 	  user_interface_(user_interface)
 {
+	database_->execute_sql(g_sql_queries[0]);
+	user_interface.printf("user database loaded\n");
 }
 
-void UserDatabase::check(std::string const &, password_hash_t const &)
+void UserDatabase::check(std::string const &name, password_hash_t const &password_hash)
 {
+	Database::results_t const result = database_->execute_sql(g_sql_queries[1], name.c_str());
+
+	user_interface_.printf("user check\n");
+
+	for (auto const &row: result)
+	{
+		for (auto const &key_value: row)
+		{
+			user_interface_.printf("%s: %s\n", key_value.first.c_str(), key_value.second.c_str());
+		}
+	}
+
 	throw std::runtime_error("STUB UserDatabase::check");
 }
 
-void UserDatabase::create(std::string const &, password_hash_t const &)
+void UserDatabase::create(std::string const &name, password_hash_t const &password_hash)
 {
+	std::string const password_hash_string(password_hash.begin(), password_hash.end());
+
+	Database::results_t const result = database_->execute_sql(g_sql_queries[2], name.c_str(), password_hash[0]);
+
+	user_interface_.printf("user create\n");
+
+	for (auto const &row: result)
+	{
+		for (auto const &key_value: row)
+		{
+			user_interface_.printf("%s: %s\n", key_value.first.c_str(), key_value.second.c_str());
+		}
+	}
+
 	throw std::runtime_error("STUB UserDatabase::create");
 }
 
-void UserDatabase::remove(std::string const &)
+void UserDatabase::remove(std::string const &name)
 {
+	Database::results_t const result = database_->execute_sql(g_sql_queries[3], name.c_str());
+
+	user_interface_.printf("user delete\n");
+
+	for (auto const &row: result)
+	{
+		for (auto const &key_value: row)
+		{
+			user_interface_.printf("%s: %s\n", key_value.first.c_str(), key_value.second.c_str());
+		}
+	}
+
 	throw std::runtime_error("STUB UserDatabase::remove");
 }
 
